e3acefcd7f0fbc753812e5668291aea06f421ea8
CalebRD33 <165441870+CalebRD33@users.noreply.github.com>
2024-06-13 19:03:59 -0700

    Endpoints
    -updated documentation.html to include endpoints
    -added requests to index.js

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
-let topMovies = [
+let movies = [
     {
-        title: 'Superbad',
-        director: 'Greg Mottola'
+        Title: 'Superbad',
+        Director: {
+            Name: 'Greg Mottola'
+        }    
     },
     {
-        title: 'Super Troopers',
-        director: 'Jay Chandrasekhar'
+        Title: 'Super Troopers',
+        Director: 'Jay Chandrasekhar'
     },
     {
-        title: 'Anchorman',
-        director: 'Adam McKay'
+        Title: 'Anchorman',
+        Director: 'Adam McKay'
     },
     {
-        title: 'Blow',
-        director: 'Ted Demme'
+        Title: 'Blow',
+        Director: 'Ted Demme'
     },
     {
-        title: 'The Nice Guys',
-        director: 'Shane Black'
+        Title: 'The Nice Guys',
+        Director: 'Shane Black'
     },
     {
-        title: 'Bourne Identity',
-        director: 'Doug Liman'
+        Title: 'Bourne Identity',
+        Director: 'Doug Liman'
     },
     {
-        title: 'Snatch',
-        director: 'Guy Ritchie'
+        Title: 'Snatch',
+        Director: 'Guy Ritchie'
     },
     {
-        title: 'Baby Driver',
-        director: 'Edgar Wright'
+        Title: 'Baby Driver',
+        Director: 'Edgar Wright'
     },
     {
-        title: 'Rush',
-        director: 'Ron Howard'
+        Title: 'Rush',
+        Director: 'Ron Howard'
     },
     {
-        title: 'The Departed',
-        director: 'Martin Scorsese'
+        Title: 'The Departed',
+        Director: 'Martin Scorsese'
     }
 ];
 
@@ -52,13 +54,67 @@ const app = express();
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'});
 app.use(morgan('combined', {stream: accessLogStream}));
 
-// returns JSON list of movies
-app.get('/movies', (reg, res) => {
-    res.json(topMovies);
-});
-
 app.get('/', (req, res) => {
     res.send('Welcome to myFlix!')
+});
+
+// returns JSON list of movies
+app.get('/movies', (req, res) => {
+    res.json(movies);
+});
+
+// READ - returns JSON object of individual movie
+app.get('/movies/:title', (req, res) => {
+    const { title } = req.params;
+    const movie = movies.find(movie => movie.Title === title);
+
+    if (movie) {
+        res.status(200).json(movie);
+    } else {
+        res.status(400).send('No movie by that name.')
+    }
+});
+
+app.get('/movies/genre/:genreName', (req, res) => {
+    const { genreName } = req.params;
+    const genre = movies.find(movie => movie.Genre.Name === genreName).Genre;
+
+    if (genre) {
+        res.status(200).json(genre);
+    } else {
+        res.status(400).send('No such genre.')
+    }
+});
+
+app.get('/movies/director/:directorName', (req, res) => {
+    const { directorName } = req.params;
+    const director = movies.find(movie => movie.Director.Name === directorName).Director;
+
+    if (director) {
+        res.status(200).json(director);
+    } else {
+        res.status(400).send('No director by that name.')
+    }
+});
+
+app.post('/users', (req, res) => {
+    res.send('Successful POST request to add new user')
+});
+
+app.put('/users/:id', (req, res) => {
+    res.send('Successful PUT request to update user info')
+});
+
+app.post('/users/:id/:movieTitle', (req, res) => {
+    res.send('Successful POST request to add a new movie to a users favorites list')
+});
+
+app.delete('/users/:id/:movieTitle', (req, res) => {
+    res.send('Successful DELETE request to delete a movie from a users favorites list')
+});
+
+app.delete('/users/:id', (req, res) => {
+    res.send('Successful DELETE request to allow existing user to deregister')
 });
 
 // retrieves static files from the public folder
