6dcc707d6ccd7163d434d615ce15ba8a579e59e7
CalebRD33 <165441870+CalebRD33@users.noreply.github.com>
2024-07-28 16:14:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ app.get('/movies', passport.authenticate('jwt', { session: false }), async (req,
         res.status(500).send('Error: ' + err);
     });
 });
-
-app.get('/users', async (req, res) => {
+/*
+app.get('/users', passport.authenticate('jwt', { session: false }), async (req, res) => {
     await Users.find()
     .then((users) => {
         res.status(201).json(users);
@@ -53,7 +53,8 @@ app.get('/users', async (req, res) => {
         console.error(err);
         res.status(500).send('Error: ' + err);
     });
-});
+}); 
+*/
 
 // READ - returns JSON object of individual movie
 app.get('/movies/:Title', passport.authenticate('jwt', { session: false }), async (req, res) => {
