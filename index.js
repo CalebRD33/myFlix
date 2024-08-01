2e0c4eccc7a68ec7f935beacf3b37911ab71574f
CalebRD33 <165441870+CalebRD33@users.noreply.github.com>
2024-08-01 15:40:10 -0700

    Update user changes
    added hashed password to update user

--- index.js
+++ index.js
@@ -44,29 +44,6 @@ app.get('/movies', passport.authenticate('jwt', { session: false }), async (req,
     });
 });
 
-app.get('/users', passport.authenticate('jwt', { session: false }), async (req, res) => {
-    await Users.find()
-    .then((users) => {
-        res.status(201).json(users);
-    })
-    .catch((err) => {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
-    });
-}); 
-
-app.get('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
-    await Users.findOne({ _id: req.params._id })
-    .then((user) => {
-        res.json(user);
-    })
-    .catch((err) => {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
-    });
-});
-
-
 // READ - returns JSON object of individual movie
 app.get('/movies/:Title', passport.authenticate('jwt', { session: false }), async (req, res) => {
     await Movies.findOne({ Title: req.params.Title })
@@ -151,7 +128,7 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false }),
     check('Password', 'Password is required').not().isEmpty(),
     check('Email', 'Email does not appear to be valid').isEmail()
 ], async (req, res) => {
-
+    let hashedPassword = Users.hashPassword(req.body.Password);
     let errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(422).json({ errors: errors.array() });
@@ -164,7 +141,7 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false }),
     {$set:
         {
             Username: req.body.Username,
-            Password: req.body.Password,
+            Password: hashedPassword,
             Email: req.body.Email,
             Birthday: req.body.Birthday
         }
